88fc72ce4d27d6b480bbebb954a2b14573c3401c
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2016-05-03 15:33:04 -0300

    Fix error preventing driver to set default mode 640x480 if no other one is set by user.

--- src/driver.c
+++ src/driver.c
@@ -197,7 +197,7 @@ NestedValidateModes(ScrnInfoPtr pScrn) {
     xf86DrvMsg(pScrn->scrnIndex, X_INFO, "Too bad for it...\n");
 
     /* If user requested modes, add them. If not, use 640x480 */
-    if (pScrn->display->modes != NULL) {
+    if (pScrn->display->modes != NULL && pScrn->display->modes[0] != NULL) {
         xf86DrvMsg(pScrn->scrnIndex, X_INFO, "User wants these modes:\n");
 
         for (i = 0; pScrn->display->modes[i] != NULL; i++) {
@@ -224,7 +224,6 @@ NestedValidateModes(ScrnInfoPtr pScrn) {
     pScrn->modePool = NULL;
 
     /* Now set virtualX, virtualY, displayWidth and virtualFrom */
-
     if (pScrn->display->virtualX >= pScrn->modes->HDisplay &&
         pScrn->display->virtualY >= pScrn->modes->VDisplay) {
         pScrn->virtualX = pScrn->display->virtualX;
